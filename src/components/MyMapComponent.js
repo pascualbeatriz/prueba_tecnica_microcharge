79643a954b991392a22df7eaff89091f6a1722d0
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-12-02 19:56:56 +0100

    Some test of MarkerClusterer

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {GoogleMap, Marker, InfoWindow} from 'react-google-maps';
+import {MarkerClusterer} from 'react-google-maps/lib/components/addons/MarkerClusterer';
 import * as birdScooters from '../clients/bird-get-scooters.json';
 import * as limeScooters from '../clients/lime-get-scooter.json';
 
@@ -9,7 +10,6 @@ class myMapComponent extends React.Component {
     this.state = {
       openVehicleId: '',
     };
-    // this.handleToggleOpen = this.handleToggleOpen.bind(this);
   }
 
   handleToggleOpen(id) {
@@ -24,30 +24,52 @@ class myMapComponent extends React.Component {
     });
   };
 
+  onMarkerClustererClick(markerClusterer) {
+    const clickedMarkers = markerClusterer.getMarkers();
+    console.log(`Current clicked markers length: ${clickedMarkers.length}`);
+    console.log(clickedMarkers);
+  }
+
   render() {
     return (
       <GoogleMap
         defaultZoom={10}
         defaultCenter={{lat: 40.416775, lng: -3.70379}}
       >
-        {birdScooters.birds.map((birdScooter, index) => (
-          <Marker
-            key={birdScooter.id}
-            position={{
-              lat: birdScooter.location.latitude,
-              lng: birdScooter.location.longitude,
-            }}
-            onClick={() => this.handleToggleOpen(birdScooter.id)}
-          >
-            {this.state.openVehicleId === birdScooter.id && (
-              <InfoWindow onCloseClick={() => this.handleToggleClose()}>
-                <div>
-                  <p>{}</p>
-                </div>
-              </InfoWindow>
-            )}
-          </Marker>
-        ))}
+        <MarkerClusterer
+          onClick={this.onMarkerClustererClick}
+          averageCenter
+          enableRetinaIcons
+          gridSize={50}
+        >
+          {birdScooters.birds.map((birdScooter, index) => (
+            <Marker
+              key={birdScooter.id}
+              position={{
+                lat: birdScooter.location.latitude,
+                lng: birdScooter.location.longitude,
+              }}
+              onClick={() => this.handleToggleOpen(birdScooter.id)}
+            >
+              {this.state.openVehicleId === birdScooter.id && (
+                <InfoWindow onCloseClick={() => this.handleToggleClose()}>
+                  <div>
+                    <p>{`Código vehículo: ${birdScooter.code}`}</p>
+                    <p>{`Marca: ${birdScooter.brand_name}`}</p>
+                    <p>{`Bluetooh: ${
+                      birdScooter.bluetooth === true ? 'Sí' : 'No'
+                    }`}</p>
+                    <p>{`Disponible: ${
+                      birdScooter.lifecycle.condition === 'available'
+                        ? 'Sí'
+                        : 'No'
+                    }`}</p>
+                  </div>
+                </InfoWindow>
+              )}
+            </Marker>
+          ))}
+        </MarkerClusterer>
       </GoogleMap>
     );
   }
